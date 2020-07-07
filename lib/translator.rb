982b01c32b8bbd8f88489d3d82e62d20ff3ac3c0
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 00:38:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,14 +14,14 @@ end
 
 
 
-def get_japanese_emoticon(file_path, new_hash)
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
 end
 
 
 
 
-def get_english_meaning (file_path, new_hash)
+def get_english_meaning (file_path, emoticon)
   # code goes here
 end
 
