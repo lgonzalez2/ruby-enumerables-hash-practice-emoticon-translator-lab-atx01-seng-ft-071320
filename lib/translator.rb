1b8c489444174370b96b1ace9d6631fac25fdbee
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 00:57:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -36,7 +36,7 @@ def get_english_meaning (file_path, emoticon)
   if english_name != ""
     p english_name
   else 
-    p "Sorry, that emoticon was not found!"
+    "Sorry, that emoticon was not found!"
   end 
 end
 
