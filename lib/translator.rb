83fd61e53537ab81b3e966f2329f36d64c312357
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 00:05:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,15 @@ require 'pry'
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-  binding.pry
+  new_hash = {}
   
-  
+  emoticons.each do |key, value|
+    new_hash << {
+      key => {:english => value[0], :japanese => value[1]}
+    }
+  end 
+   
+   new_hash
 end
 
 def get_japanese_emoticon
