8984ee45d3bb96b9ff9b1a5e0216a6b74d9cdf1d
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 01:23:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
     end 
   end 
   
-  j_emoticon = library[english_name][0]
+  j_emoticon = library[english_name][:english]
   
   if j_emoticon != ""
     p j_emoticon
