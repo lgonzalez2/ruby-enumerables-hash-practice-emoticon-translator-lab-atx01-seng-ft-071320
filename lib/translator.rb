04686b37617a4ff7383d0c50c5d078cf59681307
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-07 01:25:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
     end 
   end 
   
-  j_emoticon = library[english_name][:japanese]
+  j_emoticon = library[name][:japanese]
   
   if j_emoticon != ""
     p j_emoticon
